53d863b72f6c5528b54447108e5d27ff228d0d5c
arman <gasarman747@gmail.com>
2022-06-05 23:21:57 +0400

    Added web models. #2

---
+++ src/main/java/com/beerservice/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package com.beerservice.beerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
